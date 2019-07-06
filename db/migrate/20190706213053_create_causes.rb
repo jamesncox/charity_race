452f49d758455acb2a8fdc7baad26a56205c6c8f
James <jamesncox@yahoo.com>
2019-07-06 16:32:24 -0500

    Create Causes table

--- db/migrate/20190706213053_create_causes.rb
+++ db/migrate/20190706213053_create_causes.rb
@@ -1,4 +1,9 @@
 class CreateCauses < ActiveRecord::Migration
   def change
+    create_table :causes do |t|
+      t.string :name
+      t.integer :runner_id
+      t.integer :race_id
+    end 
   end
 end
